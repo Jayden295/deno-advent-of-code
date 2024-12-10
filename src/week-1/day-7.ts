8419d6cbfb12463eb1cc354621375bf8770676ab
Jayden <c.luong997@proton.me>
2024-12-10 21:18:18 +0100

    docs: Add SPDX license indicators on each file
    Keep forgetting them...

--- src/week-1/day-7.ts
+++ src/week-1/day-7.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 import { isNumber, OpenFileLineByLineAsArray } from "../helper.ts";
 
 function printUsage() {
--- src/week-2/day-9.ts
+++ src/week-2/day-9.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 import { isNumber, OpenFileLineByLineAsArray } from "../helper.ts";
 
 // Represents a block in a disk
